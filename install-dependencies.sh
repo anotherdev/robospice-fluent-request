82f84ef0fd21dba0cd15172e8b971353a529c9b1
Polson <polson.k@gmail.com>
2015-01-29 12:08:29 +0700

    cache android sdk components

---
+++ install-dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Fix the CircleCI path
+export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+
+DEPS="$ANDROID_HOME/installed-dependencies"
+
+if [ ! -e $DEPS ]; then
+  cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
+  echo y | android update sdk -u -a -t android-21 &&
+  echo y | android update sdk -u -a -t platform-tools &&
+  echo y | android update sdk -u -a -t build-tools-21.1.2 &&
+  echo y | android update sdk -u -a -t addon-google_apis-google-21 &&
+  echo n | android create avd -n testing -f -t android-21 &&
+  touch $DEPS
+fi
